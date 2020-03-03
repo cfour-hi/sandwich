2d447fa79051dfbc9a6e9ce10767382fdb16ff2e
monine <monine.zhou@qq.com>
2020-03-03 22:52:50 +0800

    feat: support netlify

--- src/editor/constants.js
+++ src/editor/constants.js
@@ -1,4 +1,5 @@
 export const MOBILE_PHONE_WIDTH = 375;
 export const MOBILE_PHONE_HEIGHT = 585;
 export const SANDWICH_JSON_DATA = 'SANDWICH_JSON_DATA';
-export const PREVIEW_ORIGIN = 'http://localhost:9002';
+// prettier-ignore
+export const PREVIEW_ORIGIN = process.env.SITE_DOMAIN || 'http://localhost:9002/';
